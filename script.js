b7faad298295efdfbe0e0bfd02cea38347971d1c
bouha <patrikreven.biz@gmail.com>
2025-03-26 16:03:19 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const music = document.getElementById('introMusic');
-music.muted = false;
+// Poskrbi, da bo glasba začela predvajanje ob nalaganju
+music.volume = 1.0; // Nastavi glasnost (če jo želiš prilagoditi)
+music.play().catch(err => console.log("Napaka pri predvajanju glasbe:", err));
+
 // Po 12 sekundah skrij začetno animacijo in prikaži glavni meni
 setTimeout(() => {
-  document.getElementById('intro').classList.add('hidden');
-  document.getElementById('main-content').classList.remove('hidden');
+    document.getElementById('intro').classList.add('hidden');
+    document.getElementById('main-content').classList.remove('hidden');
 }, 12000);
